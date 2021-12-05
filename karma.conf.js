c484af96a43bb0f4de68d8de3e8440257f9aec02
Clemzd <clemzd@gmail.com>
2021-12-05 19:15:31 +0100

    init

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/stragitator'),
+      dir: require('path').join(__dirname, './coverage/strategitator'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'stragitator'`, () => {
+  it(`should have as title 'strategitator'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('stragitator');
+    expect(app.title).toEqual('strategitator');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('stragitator app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('strategitator app is running!');
   });
 });
